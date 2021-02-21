486d7fa945670d9f3b8d557437c8a0877f8771d6
aliwocha <arturliwocha@gmail.com>
2021-02-21 22:43:35 +0100

    Add an option of editing order status

--- src/main/java/pl/javastart/controller/PanelController.java
+++ src/main/java/pl/javastart/controller/PanelController.java
@@ -3,13 +3,14 @@ package pl.javastart.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import pl.javastart.item.Item;
 import pl.javastart.order.Order;
 import pl.javastart.order.OrderRepository;
 import pl.javastart.order.OrderStatus;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PanelController {
@@ -32,5 +33,27 @@ public class PanelController {
         }
         model.addAttribute("orders", orders);
         return "panel/orders";
+    }
+
+    @GetMapping("/panel/zamowienia/{id}")
+    public String getOrderDetails(@PathVariable Long id, Model model) {
+        Optional<Order> order = orderRepository.findById(id);
+        return order.map(o -> addAttributes(o, model)).orElse("redirect:/");
+    }
+
+    @PostMapping("/panel/zamowienia/{id}")
+    public String changeOrderStatus(@PathVariable Long id, Model model) {
+        Optional<Order> order = orderRepository.findById(id);
+        order.ifPresent(o -> {
+            o.setStatus(OrderStatus.nextStatus(o.getStatus()));
+            orderRepository.save(o);
+        });
+        return order.map(o -> addAttributes(o, model)).orElse("redirect:/");
+    }
+
+    private String addAttributes(Order order, Model model) {
+        model.addAttribute("order", order);
+        model.addAttribute("sum", order.getItems().stream().mapToDouble(Item::getPrice).sum());
+        return "panel/orderDetails";
     }
 }
